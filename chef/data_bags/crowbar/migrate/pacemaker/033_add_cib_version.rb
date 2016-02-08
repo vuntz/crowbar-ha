f441e931e78acb72ec855d33af596929a884260b
Vincent Untz <vuntz@suse.com>
2016-02-08 09:43:22 +0100

    Revert change outside of cookbooks; the default attribute is enough

--- chef/data_bags/crowbar/migrate/pacemaker/033_add_cib_version.rb
+++
@@ -1,9 +0,0 @@
-def upgrade(ta, td, a, d)
-  a["pacemaker"]["cib_syntax_version"] = ta["pacemaker"]["cib_syntax_version"]
-  return a, d
-end
-
-def downgrade(ta, td, a, d)
-  a["pacemaker"].delete("cib_syntax_version")
-  return a, d
-end
